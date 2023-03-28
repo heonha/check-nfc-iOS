05fba7dc07dbc0c7914ed848e3c636b24db92be8
Heonjin Ha <weizen511@gmail.com>
2023-03-28 23:24:15 +0900

    Initial Commit

---
+++ checkNFC/checkNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkNFCApp.swift
+//  checkNFC
+//
+//  Created by Heonjin Ha on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct checkNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
